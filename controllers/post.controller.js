b89bb2cdfe76e7461e4fa54309dad9d46d19c1e1
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2024-11-02 15:37:01 +0200

    return post id from delete post controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -53,14 +53,14 @@ export const deletePost = async (req, res) => {
       return res.status(401).json({error: 'You are not authorized'})
     }
 
-    if(post.ing) {
+    if(post.img) {
       const imgId = post.img.split('/').pop().split('.')[0];
       await cloudinary.uploader.destroy(imgId)
     }
 
     await Post.findByIdAndDelete(req.params.id)
 
-    res.status(200).json({message: 'Post deleted successfully'})
+    res.status(200).json(req.params.id)
   } catch (error) {
     console.log("Error in deletedPost controller: ", error);
     res.status(500).json({error: "Server error"})    
